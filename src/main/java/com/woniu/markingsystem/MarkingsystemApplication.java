940c1adcfc1f92b31a537f33f2be0569b5a8ea9e
guyalin <1046087425@qq.com>
2019-09-24 15:24:02 +0800

    markingsystem first commit!

---
+++ src/main/java/com/woniu/markingsystem/MarkingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.woniu.markingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkingsystemApplication.class, args);
+    }
+
+}
